4365d49c8d7bc54ab6f9a8951d4384ff588ca04c
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-27 19:26:37 +0300

    master: update task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -8,45 +8,83 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
-            DirectoryInfo fi = new DirectoryInfo(args[0]);
-            List<FileInfo> fileList = new List<FileInfo>();
-            TimeSpan idleInterval = TimeSpan.FromMinutes(30);
-            foreach (FileInfo file in fi.GetFiles())
+            try //обработчик ошибки общего доступа к папке
             {
-                if ((DateTime.Now - file.LastAccessTime) > idleInterval)
-                {
-                    fileList.Add(file);
-                }
-            }
+                DirectoryInfo fi = new DirectoryInfo(args[0]);
+                if (!fi.Exists) return; 
+                List<FileInfo> fileList = new List<FileInfo>(); //список файлов для отложенного удаления
+                List<DirectoryInfo> dirList = new List<DirectoryInfo>(); //список директорий для отложенного удаления
+                TimeSpan timeSpentIdle = TimeSpan.FromMinutes(30);
+
+                seekObject(timeSpentIdle, ref dirList, ref fileList, ref fi);
             
-            Console.WriteLine($"Найдено {fileList.Count} файла.{Environment.NewLine}");
-            Console.WriteLine($"[Esc] - выход, [Del] - удалить, [Enter] - показать");
-            
-            ConsoleKeyInfo consoleKey;
-            do {
+                ConsoleKeyInfo consoleKey;
+
                 do {
-                    consoleKey = Console.ReadKey();
+                    Console.Clear();//выводим интерактивное меню
+                    Console.WriteLine($"Найдено {fileList.Count} файла.{Environment.NewLine}");
+                    Console.WriteLine($"[Esc] - выход, [Del] - рекурсивное удалиение файлов и папок, [Enter] - показать");
+                    do {//зацикливаем ввод для завершения выбора из предложенных вариантов
+                        consoleKey = Console.ReadKey();
+                    }
+                    while (!ConsoleKey.Escape.Equals(consoleKey.Key) && 
+                         !ConsoleKey.Enter.Equals(consoleKey.Key) && 
+                         !ConsoleKey.Delete.Equals(consoleKey.Key));
+                    switch (consoleKey.Key)//
+                    {
+                        case ConsoleKey.Delete:
+                            try//обработчик ошибки на этапе удаления
+                            {
+                            foreach(FileInfo file in fileList)
+                                { file.Delete(); };
+                            foreach(DirectoryInfo dir in dirList)
+                                {
+                                    if ((dir.GetFiles().Length == 0) && 
+                                        (dir.GetDirectories().Length == 0))
+                                    { 
+                                        dir.Delete(); 
+                                    }
+                                };
+                            }
+                            catch ( Exception e ) // вывод сообщения об ошибке удаления
+                            {
+                                Console.WriteLine($"ошибка удаления ... {e.Message}");
+                            }
+                            break;
+                        case ConsoleKey.Enter:
+                            foreach (FileInfo file in fileList)
+                            { Console.WriteLine(file.Name); };
+                            Console.WriteLine("?... нажмите любую клавишу");
+                            Console.ReadKey();
+                            break;
+                        case ConsoleKey.Escape:
+                            break;
+                    };
                 }
-                while (!ConsoleKey.Escape.Equals(consoleKey.Key) && 
-                     !ConsoleKey.Enter.Equals(consoleKey.Key) && 
-                     !ConsoleKey.Delete.Equals(consoleKey.Key));
-                switch (consoleKey.Key)
-                {
-                    case ConsoleKey.Delete:
-                        foreach(FileInfo file in fileList)
-                            { file.Delete(); };
-                        break;
-                    case ConsoleKey.Enter:
-                        foreach (FileInfo file in fileList)
-                        { Console.WriteLine(file.Name); };
-                        break;
-                    case ConsoleKey.Escape:
-                        break;
-                };
+                while (!ConsoleKey.Escape.Equals(consoleKey.Key) &&
+                !ConsoleKey.Delete.Equals(consoleKey.Key));
+                return;
             }
-            while (!ConsoleKey.Escape.Equals(consoleKey.Key) &&
-            !ConsoleKey.Delete.Equals(consoleKey.Key));
-            return;
+            catch(Exception e)
+            { Console.WriteLine($"нет доступа... {e.Message}"); }
+        }
+        public static void seekObject(TimeSpan timeAgo, ref List<DirectoryInfo> listDirs, ref List<FileInfo> listFiles, ref DirectoryInfo path)
+        {
+            foreach (FileInfo file in path.GetFiles())
+            {
+                if ((DateTime.Now - file.LastAccessTime) > timeAgo)
+                {
+                    listFiles.Add(file);
+                }
+            }
+            foreach (DirectoryInfo dir in path.GetDirectories())
+            {
+                if ((DateTime.Now - dir.LastAccessTime) > timeAgo)
+                {
+                    listDirs.Add(dir);
+                    seekObject(timeAgo, ref listDirs, ref listFiles, ref path);
+                }
+            }
         }
     }
 }
